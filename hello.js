507a70d09109acd52a7b2baa65e36c65ca186f17
Roshan Nasseri <roshan.sep26@gmail.com>
2021-03-18 00:20:20 +0000

    did a bit more testing here

--- hello.js
+++ hello.js
@@ -4,9 +4,16 @@ const sayHello  = function () {
 }
 
 console.log(sayHello());
-*/
+
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello("Caliban");
+*/
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('bil');
+console.log(greeting);
--- loopy.js
+++ loopy.js
@@ -1,4 +1,4 @@
-for (let x = 100; x <= 200; x++){
+/*for (let x = 100; x <= 200; x++){
   if (x % 3 === 0){
     console.log("Loopy")
   }
@@ -13,3 +13,28 @@ for (let x = 100; x <= 200; x++){
     console.log(x)
 }
 }
+
+//start of next section
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+
+const total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+console.log('Order total is: ', total); 
+*/
+/*
+let total = 0;
+for (let amount of amounts) {
+  total += amount;
+}
+console.log('Order total is: ', total);
+*/
+
+// declares the sayHello function
+function sayHello() {
+  var message = "Hello!"
+  return message; // returns value instead of printing it
+}
+
+console.log(sayHello());
