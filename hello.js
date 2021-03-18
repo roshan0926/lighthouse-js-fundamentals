e68c2f0e0a9b64902e27037eea7cbb31ce385f7f
Roshan Nasseri <roshan.sep26@gmail.com>
2021-03-18 00:14:05 +0000

    function testing pt 1

---
+++ hello.js
@@ -0,0 +1,12 @@
+/*
+const sayHello  = function () {
+  return "hi, world";
+}
+
+console.log(sayHello());
+*/
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
