aa6875708df29265b9bd4704854a643e052497f7
Roshan Nasseri <roshan.sep26@gmail.com>
2021-03-19 05:59:37 +0000

    other challanges that have been completed

---
+++ area.js
@@ -0,0 +1,44 @@
+/*
+function calculateRectangleArea(length, width){
+  if (length <= 0 || width <= 0){
+    return 'undefined'
+  }
+  else{
+    return (length*width)
+  }
+}
+function calculateTriangleArea(base, height){
+  if (base <= 0 || height <= 0){
+    return 'undefined'
+  }
+  else{
+    return (base*height/2)
+  }
+}
+function calculateCircleArea(radius){
+  if (radius <= 0 ){
+    return 'undefined'
+  }
+  else{
+    return (Math.PI*radius**2)
+  }
+}
+
+console.log(calculateRectangleArea(10, 5));     
+console.log(calculateRectangleArea(1.5, 2.5));  
+console.log(calculateRectangleArea(10, -5));
+
+console.log(calculateTriangleArea(10, 5)); 
+console.log(calculateTriangleArea(3, 2.5)); 
+console.log(calculateTriangleArea(10, -5)); 
+
+console.log(calculateCircleArea(10)); 
+console.log(calculateCircleArea(3.5));
+console.log(calculateCircleArea(-1));
+*/
+
+let prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+prices[0]=5.23
+prices[2]=1.23
+prices[6]=324.2
+console.log(prices)
